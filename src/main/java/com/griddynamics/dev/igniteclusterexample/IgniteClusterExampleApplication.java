d51046171e3de29af3efe3d843e0cb7064284074
Sergey Korneev <skorneev@griddynamics.com>
2018-12-25 15:02:58 +0300

    Initial commit

---
+++ src/main/java/com/griddynamics/dev/igniteclusterexample/IgniteClusterExampleApplication.java
@@ -0,0 +1,14 @@
+package com.griddynamics.dev.igniteclusterexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgniteClusterExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgniteClusterExampleApplication.class, args);
+	}
+
+}
+
